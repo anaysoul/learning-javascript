4e44432612d6658c1a03f0cfc993db1d191799fb
anaysoul <soulyana_lakew@outlook.com>
2021-06-17 20:15:33 -0400

    switching the active player

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,6 +1,8 @@
 'use script';
 
 // selecting elements
+const player0E = document.querySelector('.player--0');
+const player1E = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +33,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch players
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0E.classList.toggle('player--active');
+    player1E.classList.toggle('player--active');
   }
 });
