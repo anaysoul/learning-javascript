d5d97e670c49689b0f9a82dd63bc10873c5fa2ae
anaysoul <soulyana_lakew@outlook.com>
2021-06-18 18:53:17 -0400

    starting a new game functionality

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -12,15 +12,29 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// variables for scoping
+let scores, currentScore, activePlayer, playing;
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.remove('hidden');
+  player0E.classList.remove('player--winner');
+  player1E.classList.remove('player--winner');
+  player0E.classList.add('player--active');
+  player1E.classList.remove('player--active');
+};
+
+// start game
+init();
 
 // switch player
 const switchPlayer = function () {
@@ -75,4 +89,9 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+// start a new game
+btnNew.addEventListener('click', function () {
+  init();
 });
