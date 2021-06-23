c6233e0b144e5047b8439920fdd48df242b02d32
anaysoul <soulyana_lakew@outlook.com>
2021-06-23 18:08:20 -0400

    high level overview of javascript

---
+++ 08-how-js-works-behind-the-scenes/script.js
@@ -0,0 +1,11 @@
+// High level overview of javscript -> memory allocation
+// garbage collected -> memory management
+// Javascript is a high-level, object-oriented, multi-paradigm programming language.
+// prototype-based object-oriented
+// multi-paradigm -> an approach and mindset of structuing code, which will direct your coding style and technique (procedural, object-oriented and functional)
+// interpreted or just-in-time compiled -> abstraction over machine code; convert to machine code = compiling
+// dynamic
+// single-threaded
+// garbage-collected programing language
+// first-class functions
+// non-blocking event loop concurrency model
