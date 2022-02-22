b30cb6f50c5b56e4c2747d8838a7de9f1a0cbe34
anaysoul <soulyana_lakew@outlook.com>
2022-02-21 22:09:17 -0500

    more on strings

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,6 +3,7 @@ let currentResult = defaultResult;
 
 currentResult += 10;
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+// template literal
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
