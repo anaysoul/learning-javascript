1d1a0ac19987d4a3c4e32a075575822b3a2d032a
anaysoul <soulyana_lakew@outlook.com>
2022-02-21 22:46:48 -0500

    adding a custom function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,26 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// function
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('The result is ' + result);
+}
+add(1, 2);
+add(5, 5);
+
+function subtract(num1, num2) {
+  return num2 - num1;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num2 / num1;
+}
+
 currentResult += 10;
 
 // template literal
