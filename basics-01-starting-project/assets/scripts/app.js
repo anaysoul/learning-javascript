30403097ba838017c4b2a3f74328699586122f4f
anaysoul <soulyana_lakew@outlook.com>
2022-02-22 09:35:47 -0500

    converting data types

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,20 +3,23 @@ let currentResult = defaultResult;
 
 // function
 function add() {
-  currentResult += userInput.value;
+  currentResult += +userInput.value;
   outputResult(currentResult, '');
 }
 
-function subtract(num1, num2) {
-  return num2 - num1;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num2 / num1;
-}
-
 addBtn.addEventListener('click', add);
+
+function subtract() {
+  currentResult -= +userInput.value;
+  outputResult(currentResult, '');
+}
+
+function multiply() {
+  currentResult *= +userInput.value;
+  outputResult(currentResult, '');
+}
+
+function divide() {
+  currentResult /= +userInput.value;
+  outputResult(currentResult, '');
+}
