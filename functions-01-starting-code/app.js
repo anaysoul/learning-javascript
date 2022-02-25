40997342794fe75240623a30901ac313649df5fc
anaysoul <soulyana_lakew@outlook.com>
2022-02-24 22:09:11 -0500

    functions vs methods

---
+++ functions-01-starting-code/app.js
@@ -0,0 +1,16 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+
+const person = {
+  name: 'Max',
+  greet: function greet() {
+    console.log('Hello there!');
+  },
+};
+
+person.greet();
+
+startGameBtn.addEventListener('click', startGame);
